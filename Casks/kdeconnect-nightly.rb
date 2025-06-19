f490cb7784d5be5fc640cf30028aedc77fd2f9cb
Darwin Cereska <accfordarwin@gmail.com>
2025-06-19 02:54:30 -0400

    testing update script

--- Casks/kdeconnect-nightly.rb
+++ Casks/kdeconnect-nightly.rb
@@ -1,5 +1,5 @@
 cask "kdeconnect-nightly" do
-  version "5118"
+  version "511"
   sha256 "4e835a05cac6b7a1935ac41100690f0c5ab2f2ca35c6f159e3568d319717fc2b"
   
   url "https://cdn.kde.org/ci-builds/network/kdeconnect-kde/master/macos-arm64/kdeconnect-kde-master-#{version}-macos-clang-arm64.dmg",
